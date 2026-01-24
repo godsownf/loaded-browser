694c2e657b79e0e704f7b54a20188372f3575d19
godsownfriend601-dot <godsownfriend601@gmail.com>
2026-01-24 10:38:55 -0600

    Delete browser directory

--- browser/cookies.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export async function loadCookies(page, profileName) {
-  const path = `backend/cookies/${profileName}.json`;
-  if (!fs.existsSync(path)) return;
-
-  const cookies = JSON.parse(fs.readFileSync(path));
-  await page.setCookie(...cookies);
-}
--- browser/headers.js
+++
@@ -1,8 +0,0 @@
-export async function applyHeaders(page, profile) {
-  await page.setUserAgent(profile.USER_AGENT);
-
-  await page.setExtraHTTPHeaders({
-    ...profile.HEADERS,
-    referer: profile.REFERER
-  });
-}
